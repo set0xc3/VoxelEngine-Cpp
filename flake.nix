79c63db53331342f829a61df0b1f9003772507f9
set0xc3 <set0xc3@gmail.com>
2025-01-27 23:52:06 +0300

    update: flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,21 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-        devShells.default = with nixpkgs.legacyPackages.${system}; mkShell {
-          nativeBuildInputs = [ cmake pkg-config ];
-          buildInputs = [ glm glfw glew zlib libpng libvorbis openal luajit curl ]; # libglvnd
-          packages = [ glfw mesa freeglut entt ];
-          LD_LIBRARY_PATH = "${wayland}/lib:$LD_LIBRARY_PATH";
-        };
-    });
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+
+    developEnv = pkgs.clangStdenv;
+  in {
+    devShells.x86_64-linux.default = with pkgs;
+      pkgs.mkShell.override {stdenv = developEnv;} {
+        name = "developEnv";
+        nativeBuildInputs = [cmake pkg-config];
+        buildInputs = [glm glfw glew zlib libpng libvorbis openal luajit curl]; # libglvnd
+        packages = [glfw mesa freeglut entt ninja];
+        LD_LIBRARY_PATH = "${wayland}/lib:$LD_LIBRARY_PATH";
+      };
+  };
 }
