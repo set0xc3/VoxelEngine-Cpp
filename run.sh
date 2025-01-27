ebc0cae54ad425f36eb06d66c2a73ca24f518f7b
set0xc3 <set0xc3@gmail.com>
2025-01-27 23:48:47 +0300

    update: run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . -j$(nproc)
-cd ..
-build/VoxelEngine
+#!/bin/sh
+
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake --build build --parallel $(nproc)
+./build/VoxelEngine
